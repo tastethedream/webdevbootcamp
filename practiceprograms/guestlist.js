488116e3ab79bde416b5269a7c722daa61ecec3d
tastethedream <tastethedream@gmail.com>
2020-05-01 18:23:17 +0100

    Arrays and Loops

---
+++ practiceprograms/guestlist.js
@@ -0,0 +1,10 @@
+var guestList = ["Richard", "Dawn","Bailey", "Elliot", "Noah", "Amelia" ];
+
+var guestName = prompt ("What is your name?");
+
+if (guestList.includes (guestName)) {
+
+    alert ("Welcome!")
+} else {
+    alert ("Your name is not down so you are not coming in!");
+}
---
+++ practiceprograms/whosbuyinglunch.js
@@ -0,0 +1,19 @@
+function whosPaying(names) {
+    
+    /******Don't change the code above*******/
+        
+        //Write your code here.
+        
+        var names = ["Richard", "Dawn","Bailey", "Elliot", "Noah", "Amelia" ];
+    
+     
+        var name = Math.floor(Math.random() * names.length);
+     
+        return ( names [name] + " is going to buy lunch today!");
+        
+    
+    
+    /******Don't change the code below*******/    
+    }
+    
+    
