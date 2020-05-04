6d71c8f89a2cb231d41853367e556d240701460c
tastethedream <tastethedream@gmail.com>
2020-05-04 13:33:35 +0100

    section 11 added

---
+++ section11diceechallenge/dicee.js
@@ -0,0 +1,4 @@
+var randomNumber1 = Math.floor(Math.random()*6) +1;
+// var randomImage = [images/dice1.png, images/dice2.png, images/dice3.png,images/dice4.png, images/dice5.png, images/dice6.png ];
+
+console.log(randomNumber1)
