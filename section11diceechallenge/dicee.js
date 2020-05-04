a340787c3b9ee60a23f683041f8ca8e45baad035
tastethedream <tastethedream@gmail.com>
2020-05-04 15:56:49 +0100

    dicee challenge added

--- section11diceechallenge/dicee.js
+++ section11diceechallenge/dicee.js
@@ -1,4 +1,34 @@
 var randomNumber1 = Math.floor(Math.random()*6) +1;
-// var randomImage = [images/dice1.png, images/dice2.png, images/dice3.png,images/dice4.png, images/dice5.png, images/dice6.png ];
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+ 
 
-console.log(randomNumber1)
+document.querySelector(".img1").setAttribute("src","images/dice" + randomNumber1 + ".png");
+
+document.querySelector(".img2").setAttribute("src","images/dice" + randomNumber2 + ".png");
+ 
+if (randomNumber1 > randomNumber2){
+
+    document.querySelector("h1").textContent = "Player 1 wins!";
+
+  } else if (randomNumber1 < randomNumber2){
+
+      document.querySelector("h1").textContent = "Player 2 wins!";
+
+  } else if (randomNumber1 === randomNumber2){
+
+      document.querySelector("h1").textContent = "Draw!!";
+
+  }
+
+
+
+
+
+
+
+console.log("");
+
+
+
+
+
