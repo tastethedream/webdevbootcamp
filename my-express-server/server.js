e49af72c4614de34dfbc4389c9e2e4e125a5ff5b
tastethedream <tastethedream@gmail.com>
2020-05-09 15:02:39 +0100

    calculator js added

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -5,7 +5,19 @@ const app = express();
 app.get("/", function(request, response){
 
     response.send("Hello");
+});
+app.get("/contact", function(req, res){
+    res.send ("Contact me at:tastethedream.com");
 
+});
+
+app.get("/about", function(req, res){
+    res.send ("My name is Dawn and I am learning to build web sites");
+
+});
+
+app.get ("/socials", function (req, res){
+    res.send ("follow me on Facebook, Insta and Twitter");
 });
 
 app.listen(3000, function(){
