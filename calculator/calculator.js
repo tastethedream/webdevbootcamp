017b62178f7d27991e72b2fb19df4aa52d4ff5fb
tastethedream <tastethedream@gmail.com>
2020-05-09 17:04:36 +0100

    calculator updated

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,21 @@
 const express  = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(request, response){
+app.get("/", function(req, res){
 
-    response.send("Hello World");
+    res.sendFile (__dirname + "/index.html");
+});
+
+app.post("/", function (req, res){
+   var num1 = Number(req.body.num1);
+   var num2 = Number(req.body.num2);
+   var result = num1 + num2;
+    
+res.send("The result of your calculation is " + result);
+
 });
 
 app.listen(3000, function(){
