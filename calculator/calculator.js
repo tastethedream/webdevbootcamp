8ec02a69d609d20956f2358f4d478cfab7294152
tastethedream <tastethedream@gmail.com>
2020-05-10 09:40:38 +0100

    bmi calcultor added

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -18,6 +18,20 @@ res.send("The result of your calculation is " + result);
 
 });
 
+
+app.get ("/bmicalculator", function(req, res){
+    res.sendFile(__dirname + "bmiCalculator.html")
+});
+
+app.post ("/bmicalculator", function(req, res){
+    var height = Number(req.body.height);
+    var weight = Number(req.body.weight);
+    var result = weight / (height * height);
+    es.send("Your BMI is  " + result);
+
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 });
+
