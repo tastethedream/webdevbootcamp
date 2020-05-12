be3c64b77f5a5f3cd40c37e6b3ca4ce6c0cf589f
tastethedream <tastethedream@gmail.com>
2020-05-12 06:19:23 +0100

    newsletter signup project set up

---
+++ newsletter-signup/app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+
+
+
+
+const app = express ();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(8080, function(){
+    console.log ("Server is running on port 8080")
+
+});
