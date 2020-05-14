5ae54f2c4c3722469dff2669d904da50cbf2545a
tastethedream <tastethedream@gmail.com>
2020-05-14 17:36:22 +0100

    updated to do list

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,58 +1,46 @@
-//jshint esversion:6
+//require express and body parser
 
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+var items = ["Work out", "Eat Breakfast", "Wash dishes"];
 
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
 
-// is today saturday (6) or sunday (0)
+// todays date in english format
 
 var today = new Date();
-var currentDay = today.getDay();
-var day = "";
 
-switch (currentDay) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-    day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-    break;
-
-  default:
-    console.log ("Error: current day is equal to: " + currentDay)
-    break;
-}
-    
+var options = {
+  weekday: "long",
+  day: "numeric",
+  month: "long"
+};
+  
+var day = today.toLocaleDateString("en-US",options);
 
 //link to list.ejs file
 
-res.render("list", {
-  kindOfDay: day
+res.render("list",{kindOfDay: day, newListItems: items});
+
+});
+//send user input back and console log
+
+app.post("/", function(req, res){
+ var item = req.body.newItem;
+ items.push(item);
+ 
+  res.redirect("/");
+ 
 
 });
 
-});
+
 
 
 
