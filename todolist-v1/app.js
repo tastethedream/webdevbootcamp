5fc8519e8ae97e1df8e0fe4d4d6262fd27bb50e7
tastethedream <tastethedream@gmail.com>
2020-05-15 12:43:00 +0100

    css style sheet, header and footer added

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -4,11 +4,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
-var items = ["Work out", "Eat Breakfast", "Wash dishes"];
+let items = ["Work out", "Eat Breakfast", "Wash dishes"];
+let workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
@@ -22,32 +23,40 @@ var options = {
   month: "long"
 };
   
-var day = today.toLocaleDateString("en-US",options);
+let day = today.toLocaleDateString("en-US",options);
 
 //link to list.ejs file
 
-res.render("list",{kindOfDay: day, newListItems: items});
+res.render("list",{listTitle: day, newListItems: items});
 
 });
-//send user input back and console log
+//send user input back 
 
 app.post("/", function(req, res){
- var item = req.body.newItem;
- items.push(item);
- 
-  res.redirect("/");
- 
 
+  let item = req.body.newItem;
+
+if (req.body.list === "Work"){
+  workItems.push(item);
+  res.redirect("/work");
+} else {
+  items.push(item);
+  res.redirect("/");
+}
+ 
 });
 
+app.get ("/work", function(req, res){
+  res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
 
-
-
-
-
+app.get ("/about", function(req, res){
+  res.render("about");
+});
 
 
 
 app.listen(8080, function(){
   console.log("Server started on port 8080.");
 });
+
