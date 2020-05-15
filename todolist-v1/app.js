6a3320c86d190d83aa529a0616e9e917ecaedb55
tastethedream <tastethedream@gmail.com>
2020-05-15 14:40:05 +0100

    refactored code

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -2,10 +2,14 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
 
 const app = express();
-let items = ["Work out", "Eat Breakfast", "Wash dishes"];
-let workItems = [];
+
+const items = ["Work out", "Eat Breakfast", "Wash dishes"];
+const workItems = [];
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,17 +17,8 @@ app.use(express.static("public"));
 
 app.get("/", function(req, res){
 
-// todays date in english format
+  const day = date.getDate();
 
-var today = new Date();
-
-var options = {
-  weekday: "long",
-  day: "numeric",
-  month: "long"
-};
-  
-let day = today.toLocaleDateString("en-US",options);
 
 //link to list.ejs file
 
@@ -34,7 +29,7 @@ res.render("list",{listTitle: day, newListItems: items});
 
 app.post("/", function(req, res){
 
-  let item = req.body.newItem;
+  const item = req.body.newItem;
 
 if (req.body.list === "Work"){
   workItems.push(item);
