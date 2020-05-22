d17581b2150b09f3740ca69ecdd65c682d6f99b6
tastethedream <tastethedream@gmail.com>
2020-05-22 20:01:10 +0100

    todolistv2 connected to mongodb

--- todolist-v2/app.js
+++ todolist-v2/app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const date = require(__dirname + "/date.js");
+
 
 const app = express();
 
@@ -13,9 +13,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
-// create new mongoDB database for this application
-
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlparser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
 
 //schema
 
@@ -28,51 +26,98 @@ const itemsSchema = {
 const Item = mongoose.model("Item", itemsSchema);
 
 //add in new items to the DB
-const eat = new Item({
-  name: "eat"
+
+const item1 = new Item({
+  name: "Welcome to your todo list!"
 });
 
-const sleep = new Item({
-  name: "sleep"
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
 });
 
-const rave = new Item({
-  name: "rave"
+const item3 = new Item({
+  name: "<----- click here to delete an item"
 });
 
-const repeat = new Item({
-  name: "repeat"
-});
+// put the items in an array
+
+const defaultItems = [item1, item2, item3];
+
+//schema for alternative lists
+const listSchema = {
+  name:String,
+  items:[itemsSchema]
+};
+//mongoose model for alternative lists
+const List = mongoose.model("list", listSchema);
+  
 
 app.get("/", function(req, res) {
 
-//const day = date.getDate();
+Item.find({}, function(err, foundItems){
+//render the items in the database if the array is empty
+if (foundItems.length === 0){
+  
+  Item.insertMany(defaultItems, function(err){
+      if (err){
+        console.log (err);
+      } else {
+        console.log("successfully saved to itemsDB");
+      }
+    });  
+    res.redirect("/");  
+} else{
+  res.render("list", {listTitle: "Today", newListItems: foundItems});
+}
+});
 
-  res.render("list", {listTitle: "Today", newListItems: items});
+
+});
+//dynamic route to enable multiple lists
+
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName;
+
+const list = new List({
+  name: customListName,
+  items: defaultItems
+});
+
+list.save();
 
 });
 
 app.post("/", function(req, res){
-
-  const item = req.body.newItem;
-
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+  const item = new Item({
+    name: itemName
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+  item.save();
+  res.redirect("/");
 });
+
+app.post("/delete", function(req, res){
+  const checkedItemId = req.body.checkbox;
+
+  // deleting the checked off item using its id
+
+  Item.findByIdAndRemove(checkedItemId, function(err){
+    if (!err){
+      console.log("successfully deleted checked item");
+      res.redirect("/");
+    }
+  });
+
+});
+
+
 
 app.get("/about", function(req, res){
   res.render("about");
 });
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(8080, function() {
+  console.log("Server started on port 8080");
+
 });
