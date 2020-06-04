139ac5b84800d15ee7b384fbb802dda74d7bcdfd
tastethedream <tastethedream@gmail.com>
2020-06-04 12:45:32 +0100

    react forget me not initial commit

---
+++ react/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer(){
+
+const currentYear = new Date().getFullYear();
+
+
+return (
+
+    <footer>
+
+        <p>Copyright © Dawn Rose {currentYear} </p>
+
+    </footer>
+);
+}
+
+
+
+
+
+
+
+
+
+export default Footer;
---
+++ react/src/components/Note.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+
+function Note(){
+
+    
+    return (
+    
+    <note>
+        <div className = "note">
+            <h1>This is the title</h1>
+            <p> This is the content</p>
+        </div>
+        
+    </note>
+
+
+    );
+
+}    
+
+
+
+export default Note;
